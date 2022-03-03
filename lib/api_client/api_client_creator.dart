18591bf722890b8abf2a10d72a237a53376c3023
msakamaki <makotosakamaki127@gmail.com>
2022-03-03 11:51:43 +0900

    Android対応

--- lib/api_client/api_client_creator.dart
+++ lib/api_client/api_client_creator.dart
@@ -1,7 +1,9 @@
 import 'package:chopper/chopper.dart';
 
+import '../setting.dart';
+
 class ApiClientCreator {
-  static final String baseUrl = 'http://localhost';
+  static final String baseUrl = Setting.BASE_URL;
 
   static ChopperClient create() {
     return ChopperClient(
---
+++ lib/setting.dart
@@ -0,0 +1,6 @@
+class Setting {
+  //iOS
+  // static final String BASE_URL = 'http://localhost';
+  //Android
+  static final String BASE_URL = 'http://10.0.2.2';
+}
